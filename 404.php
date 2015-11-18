d0ae1188290dacea795cac6bc840c3d4738cbacc
awdebruijn <a.w.debruijn@gmail.com>
2015-11-18 17:23:29 +0100

    Cleanup

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package  WordPress
- * @subpackage  Preach
- * @since   Preach 0.1
- */
-
-$data = Timber::get_context();
-Timber::render( '404.twig', $data );
--- header.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package  WordPress
- * @subpackage  Preach
- * @since   Preach 0.1
- */
-
-$GLOBALS['timberContext'] = Timber::get_context();
-ob_start();
